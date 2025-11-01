b935e037b3feda0b373845e9345f7905deda8cf1
sflechel <sflechel@42.lyon.fr>
2025-11-01 15:32:42 +0100

    most errors seem fixed (but website still inaccessible)

--- requirements/wordpress/tools/wordpress.sh
+++ requirements/wordpress/tools/wordpress.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
-if ! wp core is-installed --allow-root --path=/var/www/wordpress
-then
-
 until mariadb-admin ping --skip-ssl --host=mariadb --user="$MARIADB_USER" --password="$MARIADB_PASSWORD"
 do
 	sleep 1s
 done
 
-#wp --allow-root --path=/var/www/wordpress config create \
-#	--dbname="$MARIADB_DATABASE" \
-#	--dbuser="$MARIADB_USER" \
-#	--dbpass="$MARIADB_PASSWORD" \
-#	--dbhost="mariadb" \
+#cat /var/www/wordpress/wp-config.php && 1
+
+wp config create --allow-root --path=/var/www/wordpress\
+	--dbname="$MARIADB_DATABASE" \
+	--dbuser="$MARIADB_USER" \
+	--dbpass="$MARIADB_PASSWORD" \
+	--dbhost="mariadb" \
 
 wp core install --allow-root --path=/var/www/wordpress\
 	--url="${DOMAIN_NAME}" \
 	--title="${DOMAIN_TITLE}" \
-	--admin-user="${WP_ADMIN_USER}" \
+	--admin_user="${WP_ADMIN_USER}" \
 	--admin_password="${WP_ADMIN_PASS}"\
-	--admin-email="${WP_ADMIN_EMAIL}" \
+	--admin_email="${WP_ADMIN_EMAIL}" \
 
 wp user create --allow-root --path=/var/www/wordpress\
 	"${WP_USER}" "${WP_USER_EMAIL}" \
 	--user_pass="${WP_USER_PASSWORD}" \
 	--role=editor 
 
-fi
-
 mkdir -p /run/php
-php-fpm8.4 -F
+php-fpm84 -F
